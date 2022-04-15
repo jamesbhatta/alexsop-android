291584096f4fc6f9c62c09fe95bb98f3266c334f
James Bhatta <jmsbhatta@gmail.com>
2022-04-15 10:39:23 +0545

    Initial commit

---
+++ app/src/main/java/com/alexsop/alexsop/MainActivity.java
@@ -0,0 +1,107 @@
+package com.alexsop.alexsop;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.GeolocationPermissions;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    SwipeRefreshLayout mySwipeRefreshLayout;
+    private final String siteUrl = "https://alexsop.com/android";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        mySwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swiperefresh);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDatabaseEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        webView.loadUrl(this.siteUrl);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                // callback.invoke(String origin, boolean allow, boolean remember);
+                callback.invoke(origin, true, false);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                // Check for connection
+                if(!DetectConnection.hasInternetConnection(view.getContext()))
+                {
+                    showNoConnectionDialog();
+                }
+
+                super.onReceivedError(view, request, error);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                // return super.shouldOverrideUrlLoading(view, url);
+                if(url.startsWith("tel:") || url.contains("https://api.whatsapp.com/")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mySwipeRefreshLayout.setRefreshing(false);
+
+                super.onPageFinished(view, url);
+            }
+        });
+
+        // Handle swipe refresh
+        mySwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+    }
+
+    @Override
+    public void  onBackPressed() {
+        if(webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else{
+            super.onBackPressed();
+        }
+    }
+
+    private void showNoConnectionDialog() {
+        Toast.makeText(this,"No internet connection", Toast.LENGTH_SHORT).show();
+
+        new MaterialAlertDialogBuilder(webView.getContext())
+                .setTitle("No Internet Connection")
+                .setMessage("You need to have Mobile Data or WiFi to access this. Please connect to internet and try again")
+                .setPositiveButton("Retry", (dialogInterface, i) -> {
+                    webView.reload();
+                })
+                .show();
+    }
+}
